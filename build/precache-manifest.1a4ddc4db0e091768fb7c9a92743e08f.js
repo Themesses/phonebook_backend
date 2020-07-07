2dc1aee3c34905015537665ccc304cbabefb177f
Sebastian Gugulski <sebastian.gugulski@gmail.com>
2020-07-06 23:16:05 -0400

    Modified index.js with static build middleware and a static front build

---
+++ build/precache-manifest.1a4ddc4db0e091768fb7c9a92743e08f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "160fb3c38acd7ce900520459da69ce6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "264079514670291ac12c",
+    "url": "/static/css/main.995e2195.chunk.css"
+  },
+  {
+    "revision": "7d85527c6d46c7496429",
+    "url": "/static/js/2.18364745.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.18364745.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "264079514670291ac12c",
+    "url": "/static/js/main.dc41eb8d.chunk.js"
+  },
+  {
+    "revision": "b0bc767ff50b5853565e",
+    "url": "/static/js/runtime-main.31c8eae8.js"
+  }
+]);
---
+++ build/precache-manifest.e776275459fc02e0c620ad749c233a7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e23c7c4eb6cefd92c766cf900e869ebd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc5583f3e7d6504476e4",
+    "url": "/static/css/main.995e2195.chunk.css"
+  },
+  {
+    "revision": "7d85527c6d46c7496429",
+    "url": "/static/js/2.18364745.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.18364745.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc5583f3e7d6504476e4",
+    "url": "/static/js/main.5df71b72.chunk.js"
+  },
+  {
+    "revision": "b0bc767ff50b5853565e",
+    "url": "/static/js/runtime-main.31c8eae8.js"
+  }
+]);
